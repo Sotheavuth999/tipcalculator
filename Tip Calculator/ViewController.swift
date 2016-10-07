96dc8489651f0b08ab2def40e084fbffb9c8b3ae
Sotheavuth999 <sotheavuthn@yahoo.com>
2016-10-07 15:04:54 +0300

    first commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var lblTitleLabelOUTLET: UILabel!
+    @IBOutlet weak var txtCostTextOUTLET: UITextField!
+    @IBOutlet weak var lblSliderValueLabelOUTLET: UILabel!
+    @IBOutlet weak var sldTipAmountSliderOUTLET: UISlider!
+    @IBOutlet weak var btnCalculatorButtonOUTLET: UIButton!
+    @IBOutlet weak var lblTotalCostLabelOUTLET: UILabel!
+    
+    var totalCost : Double?
+    var tipAmount : Double?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +31,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func chooseSliderValueACTION(_ sender: AnyObject) {
+        tipAmount = Double(sldTipAmountSliderOUTLET.value)
+        lblSliderValueLabelOUTLET.text = String(format: "%0.2f", tipAmount!)
+    }
+    
+    @IBAction func pressCalculateButtonACTION(_ sender: AnyObject) {
+        if !((txtCostTextOUTLET.text?.isEmpty)!) {
+            let cost = Double(txtCostTextOUTLET.text!)
+            calculateTotal(cost: cost!)
+            displayCost()
+        } else {
+            txtCostTextOUTLET.text = "Please, enter the cost!"
+        }
+        txtCostTextOUTLET.text?.removeAll()
+    }
+    
+    func calculateTotal(cost : Double) {
+        let tip = (tipAmount! / 100) * cost
+        totalCost = tip + cost
+    }
+    
+    func displayCost() {
+        lblTotalCostLabelOUTLET.text = String(format: "%0.2f", totalCost!)
+    }
 
 }
 
